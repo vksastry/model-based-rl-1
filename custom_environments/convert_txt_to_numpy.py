005474b6dd66fa742ada09b07b6fcf15ff9d83fd
Bruce Wilson <BruceWilsonUS@gmail.com>
2021-10-02 10:37:09 -0600

    Zipping.

--- custom_environments/convert_txt_to_numpy.py
+++ custom_environments/convert_txt_to_numpy.py
@@ -3,17 +3,15 @@ import pandas as pd
 
 def convert():
     # Convert text files to numpy files.
-    path = '../data/XTotal1.at'
-    pathY = path.replace(".at", ".price.at")
+    path = '../data/AT/XTotal1.at'
 
     dfFeatures = pd.read_csv(path, header=None)
     npFeatures = dfFeatures.to_numpy()
     pathOut = path.replace(".at", ".at.gz")
-    pathOut = path.replace(".at", ".at.txt")
-    np.savetxt(pathOut, npFeatures, delimiter=',', newline='\n')
+    np.savetxt(pathOut, npFeatures, delimiter=',', fmt="%.9f", newline='\n')
 
 
-    """
+    pathY = path.replace(".at", ".price.at")
     dfY = pd.read_csv(pathY, header=None, names=['historic_close', 'future_close'])
     dfY['gain'] = dfY['future_close'] / dfY['historic_close']
 
@@ -33,12 +31,9 @@ def convert():
     # Saving all of dfY worked in the past and should work now.
 
     npY = dfY.to_numpy()
-
     pathOutY = pathOut.replace(".at", ".price.at")
-
     #numpy.savetxt(pathOut, npFeatures, fmt='%.18e', delimiter=',', newline='\n', header='', footer='', comments='# ', encoding=None)
-    np.savetxt(pathOutY, npY, delimiter=',', newline='\n')
-    """
+    np.savetxt(pathOutY, npY, delimiter=',', fmt="%.9f", newline='\n')
 
 
 
