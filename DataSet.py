9738ba736960ec63d8cb8d05147d7d8c3807b2ee
Bruce Wilson <BruceWilsonUS@gmail.com>
2021-10-03 19:46:52 -0600

    950

--- DataSet.py
+++ DataSet.py
@@ -68,7 +68,7 @@ def prepare_dataloader() -> Tuple[torch.utils.data.DataLoader]:
 
 
     #download and load training data
-    BATCH_SIZE = 400
+    BATCH_SIZE = 950
     trainloader = DataLoader(train_data,
                              batch_size=BATCH_SIZE,
                              drop_last=True,
